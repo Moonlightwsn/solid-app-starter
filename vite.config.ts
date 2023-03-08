286519100d95b0af678b4ddeba1f42b06707f33e
王盛楠 <wangshengnan@wxblockchain.com>
2023-03-08 18:51:48 +0800

    feat: vite use legacy plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,35 @@
 import path from "path"
 import { defineConfig } from "vite"
 import solidPlugin from "vite-plugin-solid"
+import legacy from "@vitejs/plugin-legacy"
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    legacy({
+      targets: ["chrome 52"],
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+      renderLegacyChunks: true,
+      polyfills: [
+        "es.symbol",
+        "es.array.filter",
+        "es.promise",
+        "es.promise.finally",
+        "es/map",
+        "es/set",
+        "es.array.for-each",
+        "es.object.define-properties",
+        "es.object.define-property",
+        "es.object.get-own-property-descriptor",
+        "es.object.get-own-property-descriptors",
+        "es.object.keys",
+        "es.object.to-string",
+        "web.dom-collections.for-each",
+        "esnext.global-this",
+        "esnext.string.match-all",
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -11,8 +37,5 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-  },
-  build: {
-    target: "es2015",
   },
 })
